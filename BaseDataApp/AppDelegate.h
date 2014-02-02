7aea7c9ccd30d03e54a66e0341975d1df1ba3630
frankregel <frank.regel@gmail.com>
2014-02-02 12:46:49 +0100

    Initial Commit

---
+++ BaseDataApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BaseDataApp
+//
+//  Created by Frank on 02.02.14.
+//  Copyright (c) 2014 Frank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
